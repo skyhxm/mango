0ea6e09547dbe471f88e5dd50b3b37106b4ebb6b
skyhxm <782390603@qq.com>
2021-06-02 09:46:19 +0800

    Delete addons directory

--- addons/login_page/install.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `__PREFIX__page_login` (
-  `id` int(10) NOT NULL,
-  `title` varchar(20) NOT NULL COMMENT '名称',
-  `status` tinyint(1) unsigned DEFAULT '1' COMMENT '状态 {radio} (0:禁用,1:启用)',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='登录页面';
